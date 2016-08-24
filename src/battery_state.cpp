587f07b0ce86bf6a34ddf2ad398b7bd6518ebe42
stsssts <nagrizolich@gmail.com>
2016-08-24 22:54:39 +0300

    Made class for battery state node

--- src/battery_state.cpp
+++ src/battery_state.cpp
@@ -2,68 +2,74 @@
 #include "sensor_msgs/JointState.h"
 #include "sensor_msgs/BatteryState.h"
 
-std::vector<int> thruster_use_percent;
-
-/*
- * Функция считает удельный расход заряда батареи в секунду
- */
-float calculateBatteryCondition()
+class BatteryState
 {
-  const float k = 0.0001; // коэффициент пропорциональности
-  float p = 0; // текущая относительная мощность двигателя
-  for(unsigned int i = 0; i < thruster_use_percent.size() && !thruster_use_percent.empty(); i++)
-    p += (float) thruster_use_percent[i]/100;
-
-  return thruster_use_percent.empty() ? 0 : k * p / thruster_use_percent.size();
-}
-
-/*
- * Функция читает топик /my_robot/thruster_use
- * и записывает значения в thruster_use_percent
- */
-void thrusterUseCallback(const sensor_msgs::JointStateConstPtr & _msg)
-{
-  unsigned int i;
-
-  if(!_msg->position.empty())
+protected:
+  // Subscriber to thrusters use in percent
+  void ReadThrustersUse(const sensor_msgs::JointStateConstPtr& _msg)
   {
-    if(thruster_use_percent.empty())
+    thrusters_use_percent_ = *_msg;
+  }
+
+public:
+  BatteryState(ros::NodeHandle &ros_node)
+  {
+    // init publishers
+    battery_state_publisher_ = ros_node.advertise<sensor_msgs::BatteryState>("/my_robot/battery_state", 1);
+
+    // battery present and full
+    battery_msg_.percentage = 100;
+    battery_msg_.present = 1;
+
+    // init subscribers
+    thruster_use_subscriber_ = ros_node.subscribe("/my_robot/thruster_use", 1, &BatteryState::ReadThrustersUse, this);
+  }
+
+  // Функция считает удельный расход заряда
+  // батареи в секунду и вычитает из имеющегося
+  void calculateBatteryCondition()
+  {
+    const float k = 0.0001; // коэффициент пропорциональности
+    float p = 0; // текущая относительная мощность двигателей
+
+    for(unsigned int i = 0; i < thrusters_use_percent_.position.size(); i++)
+      p += thrusters_use_percent_.position[i]/100;
+
+    if (!thrusters_use_percent_.position.empty() && battery_msg_.percentage > 0)
     {
-      for(i = 0; i < _msg->position.size(); i++)
-        thruster_use_percent.push_back(_msg->position[i]);
-    }
-    else
-    {
-      for(i = 0; i < _msg->position.size(); i++)
-        thruster_use_percent[i] = _msg->position[i];
+      battery_msg_.percentage -= (100 * k * p /thrusters_use_percent_.position.size());
     }
   }
-}
+
+  void Publish()
+  {
+    battery_state_publisher_.publish(battery_msg_);
+  }
+
+protected:
+  // publishers and messages to be published
+  ros::Publisher battery_state_publisher_;
+  sensor_msgs::BatteryState battery_msg_;
+
+  // subscribers and received messages
+  ros::Subscriber thruster_use_subscriber_;
+  sensor_msgs::JointState thrusters_use_percent_;
+};
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "battery_condition");
-  ros::NodeHandle rosnode;
-
-  ros::Subscriber thruster_use_subscriber = rosnode.subscribe("/my_robot/thruster_use", 1, thrusterUseCallback);
-  ros::Publisher battery_state_publisher = rosnode.advertise<sensor_msgs::BatteryState>("/my_robot/battery_state", 1);
+  ros::NodeHandle ros_node;
 
   ros::Rate loop_rate(1); // 1 hz
 
-  float battery_filling = 1.0;
-  sensor_msgs::BatteryState battery_msg;
-  battery_msg.present = true;
+  // Battery state class
+  BatteryState batteryState(ros_node);
 
   while (ros::ok())
   {
-    if(battery_filling > 0)
-      battery_filling -= calculateBatteryCondition();
-    else
-      battery_filling = 0;
-
-    // Publish battery condition
-    battery_msg.percentage = battery_filling * 100;
-    battery_state_publisher.publish(battery_msg);
+    batteryState.calculateBatteryCondition();
+    batteryState.Publish();
 
     ros::spinOnce();
     loop_rate.sleep();
